19aa2071537cab0c7b5ed4b5a2bed3ec6e5a6be3
Aayush Garg <aayushgarg2006@gmail.com>
2022-10-21 19:50:53 +0530

    final lb

--- routers/leaderboard.js
+++ routers/leaderboard.js
@@ -1,34 +1,8 @@
 const router = require("express").Router();
-const User = require("../models/userSchema");
-const ejs = require("ejs");
-var fs = require("fs");
-
-var lastRender = Date('00');
 
 router.get("/", async (req, res) => {
   //update every 5 minutes
-  await updateLeaderboard();
   res.render("pages/leaderboardinfuse", { user: req.user });
 });
-updateLeaderboard()
-async function updateLeaderboard() {
-  if (Date.now() - lastRender < 300000) {
-    console.log("Leaderboard not updated, too soon");
-    return;
-  } else {
-    var users = await User.find({ admin: false })
-    users.sort(function (a, b) {
-      return b.plat_levels_completed.length - a.plat_levels_completed.length;
-    });
-    var file = await ejs.renderFile(
-      __dirname + "/../views/pages/leaderboard.ejs",
-      { users: users }
-    );
-    //store file in public folder
-    fs.writeFileSync(__dirname + "/../public/leaderboard.html", file);
-    lastRender = Date.now();
-    return;
-  }
-}
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,123 +3,28 @@
 require('dotenv').config()
 
 const express = require('express'),
+path = require('path'),
     app = express(),
-    mongoose = require('mongoose'),
-    session = require("cookie-session"),
-    path = require('path'),
-    cookieParser = require("cookie-parser"),
-    expressLayouts = require('express-ejs-layouts'),
-    passport = require('passport');
+    expressLayouts = require('express-ejs-layouts');
 
 const port = process.env.PORT || 5100,
-    passport_init = require('./utilities/passport'),
-    backlinks = require('./utilities/backlinks.json'),
-    bloatRouter = require('./routers/bloat'),
     landingRouter = require('./routers/landing'),
-    playRouter = require('./routers/play'),
-    adminRouter = require('./routers/admin'),
-    profileRouter = require('./routers/profile'),
-    leaderboardRouter = require('./routers/leaderboard'),
-    authRouter = require("./routers/auth");
-
-if (process.env.NODE_ENV === 'production') {
-    app.enable('trust proxy');
-}
-else {
-    app.disable('trust proxy');
-}
-
-app.use((req, res, next) => {
-    if (req.headers.hasOwnProperty('x-forwarded-proto') && req.headers['x-forwarded-proto'].toString() !== 'https' && process.env.NODE_ENV === 'production') {
-        res.redirect('https://' + req.headers.host + req.url);
-    }
-    else {
-        next();
-    }
-})
+    leaderboardRouter = require('./routers/leaderboard');
 
 //ejs
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')))
 //mongo
-const db = process.env.MONGO_URI;
 
-//passportJs
-if (process.env.NODE_ENV === 'production') {
-    app.use(session({
-        secret: process.env.SECRET,
-        resave: true,
-        saveUninitialized: true,
-        sameSite: 'none',
-        overwrite: true,
-        secure: true,
-        maxAge: 1000 * 60 * 60 * 24 * 7
-    }));
-} else {
-    app.use(session({
-        secret: process.env.SECRET,
-        resave: true,
-        saveUninitialized: true,
-    }));
-}
-
-app.use(cookieParser(process.env.SECRET));
 app.use(expressLayouts);
 
-app.get('*', (req, res, next) => {
-    passport_init(passport, req);
-    next();
-})
-
-//initializing passport
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use(bloatRouter);
 app.use('/', landingRouter);
-app.use("/auth", authRouter)
-app.use('/play', playRouter)
-app.use('/admin', adminRouter)
-app.use('/profile', profileRouter)
 app.use('/leaderboard', leaderboardRouter)
 
 app.get('/404', (req, res) => {
     res.render('404', { user: req.user });
 })
 
-app.get('/:id', (req, res, next) => {
-    const id = req.params.id;
-    const link = backlinks.find(link => link.link === id);
-    if (link) {
-        if (link.backlink) {
-            return res.redirect(link.backlink);
-        }
-        if (link.text) {
-            return res.render('hiddentext', { link });
-        }
-    }
-    else {
-        res.redirect('/404');
-    }
-});
-
-// app.use((err, req, res, next) => {
-//     ReportCrash(err.stack.toString())
-//     ReportWebVital("App Has Crashed, Please Check The Logs, Trying To Restart On My Own!");
-//     next(err)
-// })
-
-mongoose.connect(db, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
-    // ReportWebVital(`Connected to Mongo DB`);
-    console.log("Connected to Mongo DB")
-    app.listen(port, () => {
-        // ReportWebVital(`TS Prog listening at port ${port}`);
-        console.log(`TS encryptid listening at http://localhost:${port}`)
-    })
-}).catch(err => {
-    // ReportCrash(err.stack.toString())
-    // ReportWebVital("App Has Crashed, Please Check The Logs, Trying To Restart On My Own!");
+app.listen(port, () => {
+    console.log(`TS encryptid listening at http://localhost:${port}`)
 })
